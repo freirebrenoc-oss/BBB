adf0f12dd6ac704b1311db5b73393ca3f8a465c8
Breno Candido <freirebreno.c@gmail.com>
2025-10-06 15:23:08 -0300

    teste.py

--- teste.py
+++ teste.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.write("Alô mundo")
 st.title('saudação')
-nome = st.text_input("Digite o seu nome":)
+nome = st.text_input("Digite o seu nome")
 if nome:
   st.write(nome.upper())
