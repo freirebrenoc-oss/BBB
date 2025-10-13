c53ddbcf363c46bbd021c6389bb17dd005c4a6cb
Breno Candido <freirebreno.c@gmail.com>
2025-10-13 15:11:26 -0300

    paises.py

--- paises.py
+++ paises.py
@@ -4,5 +4,10 @@ import plotly.express as px
 
 dataset = pd.read_csv('https://www.irdx.com.br/media/uploads/paises.csv')
 
-st.dataframe(dataset)
-
+fig = px.choropleth(dataset,
+                    locations=dataset['iso3'],
+                    color=dataset['longitude'],
+                    hover_name=dataset['nome'])
+fig.update_layout(title='Mapa Coroplético dos Países',
+                  geo_scope='world')
+st.plotly_chart(fig, use_container_width=True, theme="streamlit")
